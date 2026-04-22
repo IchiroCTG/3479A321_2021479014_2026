5a411c713b764e08a87e4c8f97306c7e9f2c9313
IchiroCTG <rementalize@gmail.com>
2026-04-22 19:11:56 -0400

    Implementacion de Listas

--- lib/ui/screens/HistoryScreen.dart
+++ lib/ui/screens/HistoryScreen.dart
@@ -6,24 +6,30 @@ class HistoryScreen extends StatelessWidget {
   
   @override 
   Widget build(BuildContext context) { 
-    final GameResult game = GameResult(date: '2024-06-01', timeSpent: '5m 30s', isVictory: false); // Datos de ejemplo para mostrar en la maqueta
+    final List<GameResult> gameHistory = [ 
+      GameResult(date: '2024-06-01', timeSpent: '5m 30s', isVictory: true, difficulty: 'easy'), 
+      GameResult(date: '2024-06-02', timeSpent: '3m 45s', isVictory: false, difficulty: 'medium'),
+      GameResult(date: '2024-06-03', timeSpent: '4m 15s', isVictory: true, difficulty: 'hard'),
+      GameResult(date: '2024-06-04', timeSpent: '3m 15s', isVictory: false, difficulty: 'medium'),
+      GameResult(date: '2024-06-05', timeSpent: '5m 30s', isVictory: true, difficulty: 'easy'), 
+      GameResult(date: '2024-06-06', timeSpent: '3m 45s', isVictory: false, difficulty: 'hard'),
+      GameResult(date: '2024-06-07', timeSpent: '4m 15s', isVictory: true, difficulty: 'hard'),
+      GameResult(date: '2024-06-08', timeSpent: '3m 15s', isVictory: false, difficulty: 'medium'),
+    ]; // Datos de ejemplo para mostrar en el historial, en una aplicación real esto vendría de una base de datos o almacenamiento local
     return Scaffold( 
       appBar: AppBar(title: const Text('Historial (Maqueta)')),
-      body: Center(
-        child: Card(
-          color: game.isVictory ? Colors.green[100] : Colors.red[100],
-          elevation: 4,
-          child: Padding(
-            padding: EdgeInsets.all(16.0),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text('Fecha: ${game.date}'), Text('Tiempo: ${game.timeSpent}')
-              ],
-            )
-          )
-        ),
-      )
+      body: ListView.builder(
+        itemCount: gameHistory.length,
+        itemBuilder: (context, index) {
+          final result = gameHistory[index];
+          return ListTile(
+            leading: Icon(result.isVictory ? Icons.check_circle : Icons.cancel, color: result.isVictory ? Colors.green : Colors.red),
+            title: Text('Fecha: ${result.date} - Dificultad: ${result.difficulty}'),
+            subtitle: Text('Tiempo: ${result.timeSpent}'),
+            
+          );
+        },
+      ),
     );
   } 
 }
